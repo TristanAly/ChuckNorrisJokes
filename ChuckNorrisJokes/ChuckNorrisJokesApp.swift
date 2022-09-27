5015563fdadeefe9b23dc518e06eefa808b40861
Tristan Aly <tristan.aly@gmail.com>
2022-09-27 09:46:21 +0200

    Initial Commit

---
+++ ChuckNorrisJokes/ChuckNorrisJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChuckNorrisJokesApp.swift
+//  ChuckNorrisJokes
+//
+//  Created by apprenant1 on 27/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChuckNorrisJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
